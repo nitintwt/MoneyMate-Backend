f87d9ff29e38d2152c05a7d217f95277b2937dd5
Nitin Singh <nitintwt27@gmail.com>
2025-03-31 02:22:18 +0530

    add auth controller and routes

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,4 +1,6 @@
 import mongoose , {Schema} from "mongoose";
+import jwt from 'jsonwebtoken'
+import bcrypt from 'bcrypt'
 
 const userSchema = new Schema(
   {
@@ -41,5 +43,44 @@ const userSchema = new Schema(
     timestamps: true
   }
 )
+
+// here we care encrypting our password just brfore saving the user data, using pre hook of mongoose
+userSchema.pre("save", async function (next) {
+  if(!this.isModified("password")) return next();  // if the password is not modified , just direct save the data without encrypting
+
+  this.password = await bcrypt.hash(this.password, 10)
+  next()
+})
+
+userSchema.methods.isPasswordCorrect = async function(password){
+  return await bcrypt.compare(password, this.password)
+}
+
+userSchema.methods.generateAccessToken = function (){
+  return jwt.sign(
+    {
+      _id:this._id,
+      email:this.email,
+      name:this.name,
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+      expiresIn:process.env.ACCESS_TOKEN_EXPIRY
+    }
+  )
+}
+
+userSchema.methods.generateRefreshToken = function(){
+  return jwt.sign(
+      {
+          _id: this._id,
+          
+      },
+      process.env.REFRESH_TOKEN_SECRET,
+      {
+          expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+      }
+  )
+}
 
 export const User = mongoose.model("User" , userSchema)
